2cef5164a0654cea932c0e93b0e8d68c3570f0ca
Petar <petar.nikolaev@dnhsoft.com>
2018-08-01 18:07:22 +0300

    login add user and fetch data

--- src/public/form_process.php
+++ src/public/form_process.php
@@ -13,23 +13,17 @@ if (isset($_POST['submit'])) {
     $usernameLength = strlen($username);
 
     if ($usernameLength < $min) {
-
         echo "Username must contain more than 5 symbols";
-
     }
 
     if ($usernameLength > $max) {
-
         echo "Username must contain less than 10 symbols";
-
     }
 
     if (!in_array($username, $names)) {
-
         echo " Invalid Username";
 
     } else {
-
         echo "Welcome";
 
     }
---
+++ src/public/mysql/login_read.php
@@ -0,0 +1,43 @@
+<?php
+
+    $connection = new PDO("mysql:host=mysqldb;dbname=loginapplication;charset=UTF8", 'root', '123456');
+
+    if ($connection) {
+        echo 'We are connected ';
+    }
+    else {
+        die('Database connection failed ');
+    }
+
+$statement = $connection->query("SELECT * FROM users");
+while ($row = $statement->fetch()) {
+    echo "<br />\n";
+    echo $row['username']." : ";
+    echo $row['password']."<br />\n";
+}
+
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+</head>
+<body>
+
+<div class="container">
+    <div class="col-sm-6">
+
+    </div>
+</div>
+
+</body>
+</html>
